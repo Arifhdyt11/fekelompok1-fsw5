ddeb3ff456fcad41c46a58f383b0aaf8dc09a840
muhamadazis32 <muhamadazis9a@gmail.com>
2022-06-14 22:35:21 +0700

    View - Login

--- src/pages/components/IconText.js
+++ src/pages/components/IconText.js
@@ -2,7 +2,7 @@ import Button from "../../elements/Button";
 
 export default function iconText() {
   return (
-    <Button className="brand-text-icon" href="" type="link">
+    <Button className="brand-text-icon" href="/" type="link">
       Shoes<span className="text-gray-900">narian.</span>
     </Button>
   );
