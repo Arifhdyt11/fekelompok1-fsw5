10e5444f9d6bf7b77bdc31da94ead471f7034c02
Muhamad Arif Hidayat <63541777+Arifhdyt11@users.noreply.github.com>
2022-06-14 17:20:38 +0700

    Update Navbar

---
+++ src/pages/components/IconText.js
@@ -0,0 +1,9 @@
+import Button from "../../elements/Button";
+
+export default function iconText() {
+  return (
+    <Button className="brand-text-icon" href="" type="link">
+      Shoes<span className="text-gray-900">narian.</span>
+    </Button>
+  );
+}
--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,33 +1,32 @@
-import React from "react";
+import Button from "../../elements/Button";
+import BrandIcon from "./IconText";
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
-    <div className="header py-2">
+    <nav className="navbar navbar-expand-lg navbar-light py-3">
       <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              NavbarLogo
-            </a>
-            <div
-              className="collapse navbar-collapse "
-              id="navbarSupportedContent"
-            >
-              <form className="d-flex ms-5">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-              </form>
-              <button className="btn btn-login ms-auto px-3 py-2" type="submit">
-                Masuk
-              </button>
-            </div>
-          </div>
-        </nav>
+        <BrandIcon />
+        <div className="collapse navbar-collapse " id="navbarSupportedContent">
+          <form className="d-flex ms-5">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+          </form>
+
+          <Button
+            className="btn btn-login ms-auto px-3 py-2"
+            hasShadow
+            isPrimary
+            href="/login"
+            type="link"
+          >
+            Masuk
+          </Button>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 }
