bbc9cfd262f65ae3f3521146b12eba876cc06822
Audima Oktasena <audimaokta@gmail.com>
2022-06-15 14:58:36 +0700

    Menambah Fitur Profile

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import "./assets/css/style.css";
 
 import LandingPage from "./pages/LandingPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProfilePage from "./pages/ProfilePage";
+import InfoProdukPage from "./pages/InfoProdukPage";
+import InfoPenawarPage from "./pages/InfoPenawarPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/seller/product" element={<InfoProdukPage />} />
+        <Route path="/transaction" element={<InfoPenawarPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+
+export default function ProfilePage() {
+  return (
+    <div>
+      <Navbar />
+      <Profile />
+      <Footer />
+    </div>
+  );
+}
