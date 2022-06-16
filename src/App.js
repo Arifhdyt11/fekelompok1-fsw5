a7a3b962d5bc0409a236c7849fefe34377f3e478
Audima Oktasena <audimaokta@gmail.com>
2022-06-16 20:42:39 +0700

    Fitur info penawar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from "./pages/Login";
 import DaftarJual from "./pages/DaftarJual";
 import DetailProduct from "./pages/DetailProduct";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import InfoPenawarPage from "./pages/InfoPenawar";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/product/:id" element={<DetailProduct />} />
         <Route path="/seller/daftar-jual" element={<DaftarJual />} />
+        <Route path="/transaction" element={<InfoPenawarPage />} />
         <Route
           path="/seller/product/:id"
           element={<DetailProduct isSeller="yes" />}
---
+++ src/components/info-penawar/BuyerInfoPenawar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import imgCust from "../../assets/images/img-infoPenawar2.jpg";
+import "../../assets/css/infoPenawar.css";
+
+export default function BuyerInfoPenawar(props) {
+  return (
+    <div className="mt-4" id="buyerInfoPenawar">
+      {props.databuyer.map((item) => (
+        <div key={item} className="card shadow">
+          <div className="card-body">
+            <div className="row gx-5 customer" id="row">
+              <div className="col-auto ">
+                <div>
+                  <img
+                    className="imageProfile"
+                    src={imgCust}
+                    alt="Customer 1"
+                  />
+                </div>
+              </div>
+              <div className="col">
+                <div className="me-auto mb-2">
+                  <p className="buyerInfoPenawar">
+                    {item.name}
+                    <label className="customerTag">Pembeli</label>
+                  </p>
+                  <p className="customerCity">{item.city}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
