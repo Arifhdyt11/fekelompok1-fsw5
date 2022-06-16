c8931c82c3f417e32bc1bdfeafdec239493476ba
Audima Oktasena <audimaokta@gmail.com>
2022-06-16 12:09:48 +0700

    Penambahan feature profile

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LandingPage from "./pages/LandingPage";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import DaftarJual from "./pages/DaftarJual";
+import ProfilePage from "pages/Profile";
 import DetailProduct from "./pages/DetailProduct";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -16,6 +17,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/product/:id" element={<DetailProduct />} />
         <Route path="/seller/daftar-jual" element={<DaftarJual />} />
+        <Route path="/profile" element={<ProfilePage />} />
+
         <Route
           path="/seller/product/:id"
           element={<DetailProduct isSeller="yes" />}
