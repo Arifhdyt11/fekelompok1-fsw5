72cdcc2881e74d9b5447726b1dcdc9b92876662d
Muhamad Arif Hidayat <63541777+Arifhdyt11@users.noreply.github.com>
2022-06-23 00:30:55 +0700

    Add feature onClick Button CTA

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,30 @@
 import Button from "elements/Button";
+
 import { getInitialData } from "json/data.js";
+import { getKategoriData } from "json/kategori-produk";
+
 import img from "assets/images/ilustrasi.svg";
+import { useState } from "react";
 
 export default function Product(props) {
+  // getdata
+  const [product, setProduct] = useState(getInitialData());
+  const [kategori] = useState(getKategoriData());
+
+  const menuItems = [...new Set(getKategoriData().map((Val) => Val.category))];
+
+  const filterItem = (curcat) => {
+    const newItem = getInitialData().filter((newVal) => {
+      return newVal.category === curcat;
+    });
+    setProduct(newItem);
+  };
   return (
     <>
-      <section className="container section-product mt-2 mb-5">
+      <section
+        className="container section-product mt-2 mb-5"
+        ref={props.refCallToAction}
+      >
         <div className="filter mb-5">
           <h3>Kategori</h3>
           <div className="justify-content-start my-2">
@@ -15,12 +34,12 @@ export default function Product(props) {
               isSecondary
               href="/"
               type="link"
-              onClick={() => props.setProduct(getInitialData())}
+              onClick={() => setProduct(getInitialData())}
             >
               All
             </Button>
 
-            {props.menuItems.map((kategori, index) => {
+            {menuItems.map((kategori, index) => {
               return (
                 <Button
                   className="btn btn-filter me-3 my-2"
@@ -28,7 +47,7 @@ export default function Product(props) {
                   isSecondary
                   href=""
                   type="link"
-                  onClick={() => props.filterItem(kategori)}
+                  onClick={() => filterItem(kategori)}
                   key={index}
                 >
                   {kategori}
@@ -39,7 +58,7 @@ export default function Product(props) {
         </div>
         <div className="product">
           <div className="row justify-content-center">
-            {props.data.length === 0 ? (
+            {product.length === 0 ? (
               <div className="d-flex justify-content-center null-illustration p-5">
                 <div>
                   <img src={img} alt="" className="img-fluid mb-3" />
@@ -47,7 +66,7 @@ export default function Product(props) {
                 </div>
               </div>
             ) : (
-              props.data.map((item) => {
+              product.map((item) => {
                 return (
                   <Button
                     type="link"
