8ad400140ad4a37abe2494e0a1ea457a743ad973
Muhamad Arif Hidayat <63541777+Arifhdyt11@users.noreply.github.com>
2022-06-26 10:26:21 +0700

    Add Notification

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,5 +1,27 @@
-import React from "react";
+import { useState } from "react";
+
+import Notifications from "react-notifications-menu";
+
+const DEFAULT_NOTIFICATION = {
+  image:
+    "https://cutshort-data.s3.amazonaws.com/cloudfront/public/companies/5809d1d8af3059ed5b346ed1/logo-1615367026425-logo-v6.png",
+  message: "Notification one.",
+  detailPage: "/events",
+  receivedTime: "12h ago",
+};
 
 export default function Notification() {
-  return <div>Notification</div>;
+  const [data] = useState([DEFAULT_NOTIFICATION]);
+  return (
+    <Notifications
+      data={data}
+      header={{
+        title: "Notifications",
+        option: { text: "View All", onClick: () => console.log("Clicked") },
+      }}
+      markAsRead={(data) => {
+        console.log(data);
+      }}
+    />
+  );
 }
