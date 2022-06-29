ed0e8e1fb257c490a704dcf3f4681b7b9997b22a
muhamadazis32 <muhamadazis9a@gmail.com>
2022-06-29 18:40:03 +0700

    fix bug IconText.js

--- src/components/IconText.js
+++ src/components/IconText.js
@@ -2,11 +2,12 @@ import Button from "elements/Button";
 import { useSelector } from "react-redux";
 
 export default function IconText() {
-  const { user } = useSelector((state) => state.AuthReducer);
+
+  const { user, isAuthenticated } = useSelector((state) => state.AuthReducer);
   return (
     <Button
       className="brand-text-icon"
-      href={user.data.role === "SELLER" ? "/seller" : "/"}
+      href={ isAuthenticated? user.data.role ==="SELLER"? "/seller" : "/" : "/"}
       type="link"
     >
       Shoes<span className="text-gray-900">narian.</span>
