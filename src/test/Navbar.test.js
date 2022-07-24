76c90b5b2bd4a3ca8c2d1c3ab1f8c2f49a25d225
Audima Oktasena <audimaokta@gmail.com>
2022-07-24 09:33:58 +0700

    git pull origin

--- src/test/Navbar.test.js
+++ src/test/Navbar.test.js
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import store from "../store";
 import { MemoryRouter as Router } from "react-router-dom";
 import Navbar from "components/Navbar";
+import Footer from "components/Footer";
 
 window.scrollTo = jest.fn();
 
@@ -18,4 +19,25 @@ describe("IconText", () => {
     );
     expect(screen.getAllByText(/narian/)).toBeTruthy();
   });
+
+  it("Mengecek text di Footer", () => {
+    render(
+      <Router>
+        <Footer />
+      </Router>
+    );
+    expect(screen.getAllByText(/Be Trend And In Style/)).toBeTruthy();
+  });
+
+  // const MOCK_SUBMIT_BTN = jest.fn();
+  // it("Cek button di Footer ", () => {
+  //   render(
+  //     <Router>
+  //       <Footer onSubmit={MOCK_SUBMIT_BTN} />
+  //     </Router>
+  //   );
+  //   const btnRegist = screen.queryByTestId("footer");
+  //   fireEvent.click(btnRegist);
+  //   expect(MOCK_SUBMIT_BTN).toBeTruthy();
+  // });
 });
